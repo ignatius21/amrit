fc17b3d08770a4bf5ec9acf6d9e2b1f5b1612fb0
theopharnell <98174756+theopharnell@users.noreply.github.com>
2023-02-27 15:41:56 -0300

    agregado de onClick al footer

--- src/components/Navegacion2.jsx
+++ src/components/Navegacion2.jsx
@@ -7,16 +7,16 @@ function Navegacion2() {
         <nav className='navbar justify-content-center mt-3'>
             <ul className="nav mt-2 footer2">
                 <li className="nav-item">
-                    <Link className="nav-link cta2" aria-current="page" to="/contacto"><span className='hover-underline-animation2'>contacto</span></Link>
+                    <Link className="nav-link cta2" aria-current="page" to="/contacto" onClick={() => window.scrollTo(0, 0)}><span className='hover-underline-animation2'>contacto</span></Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link cta2" to="/productos"><span className='hover-underline-animation2'>productos</span></Link>
+                    <Link className="nav-link cta2" to="/productos" onClick={() => window.scrollTo(0, 0)}><span className='hover-underline-animation2'>productos</span></Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link cta2" to="/nosotros"><span className='hover-underline-animation2'>nosotros</span></Link>
+                    <Link className="nav-link cta2" to="/nosotros" onClick={() => window.scrollTo(0, 0)}><span className='hover-underline-animation2'>nosotros</span></Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link cta2" to="/galeria"><span className='hover-underline-animation2'>galeria</span></Link>
+                    <Link className="nav-link cta2" to="/galeria" onClick={() => window.scrollTo(0, 0)}><span className='hover-underline-animation2'>galeria</span></Link>
                 </li>
             </ul>
         </nav>
